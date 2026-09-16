ceb9cb4d6fa17c06278601af7c0dd8cc5d6399bb
luisOrtga <ortegaluiskb@gmail.com>
2026-09-16 12:47:04 -0600

    Add env and server

--- class_1/http.js
+++ class_1/http.js
@@ -1,12 +1,15 @@
 import http from 'node:http'
+import { findAvailablePort } from './free-ports.js'
+
+const desiredPort = process.env.PORT ?? 3000
 
 const server = http.createServer((req, res) => {
   console.log(`request received`)
   res.end('Hello World')
 })
 
-server.listen(0, () => {
-  console.log(
-    `Server listening on port http://localhost:${server.address().port}`,
-  )
+findAvailablePort(desiredPort).then((port) => {
+  server.listen(port, () => {
+    console.log(`Server listening on port http://localhost:${port}`)
+  })
 })

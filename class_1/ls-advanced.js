4000dcabc96383051d840f7d114843d8cbf36e18
luisOrtga <ortegaluiskb@gmail.com>
2026-09-16 10:27:37 -0600

    ready optionals args

--- class_1/ls-advanced.js
+++ class_1/ls-advanced.js
@@ -1,16 +1,39 @@
 import fs from 'node:fs/promises'
+import path from 'node:path'
 
 const folder = process.argv[2] ?? '.'
 
-fs.readdir(folder)
-  .then((files) => {
-    files.forEach((file) => {
-      console.log(file)
-    })
-  })
-  .catch((err) => {
-    if (err) {
-      console.error('Error to read directory : ', err)
-      return
+async function ls(folder) {
+  let files
+  try {
+    files = await fs.readdir(folder)
+  } catch {
+    console.error(`Not read directory ${folder}`)
+    process.exit(1)
+  }
+
+  const filesPromises = files.map(async (file) => {
+    const filePath = path.join(folder, file)
+    let stats
+
+    try {
+      stats = await fs.stat(filePath) // ----> status : information of file
+    } catch {
+      console.error(`The file could not be read`)
+      process.exit(1)
     }
+
+    const isDirectory = stats.isDirectory()
+    const fileType = isDirectory ? 'd' : 'f'
+    const fileSize = stats.size
+    const fileModified = stats.mtime.toDateString()
+
+    return `${fileType} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} ${fileModified}`
   })
+
+  const filesInfo = await Promise.all(filesPromises)
+
+  filesInfo.forEach((fileinfo) => console.log(fileinfo))
+}
+
+ls(folder)

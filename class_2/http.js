3ba53d5f457c75303fcdc0cb7c7dc940566f12cd
luisOrtga <ortegaluiskb@gmail.com>
2026-09-16 16:42:00 -0600

    second class

---
+++ class_2/http.js
@@ -0,0 +1,14 @@
+import http from 'node:http'
+
+const desiredPort = process.env.PORT ?? 3000
+
+const server = http.createServer((req, res) => {
+  console.log(`request received`)
+  res.end('Hello World')
+})
+
+findAvailablePort(desiredPort).then((port) => {
+  server.listen(port, () => {
+    console.log(`Server listening on port http://localhost:${port}`)
+  })
+})

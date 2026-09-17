957de1422de82921bbb073aa8b43c1ec5c96807c
luisOrtga <ortegaluiskb@gmail.com>
2026-09-16 20:16:28 -0600

    Add server http 404

--- class_2/http.js
+++ class_2/http.js
@@ -2,13 +2,27 @@ import http from 'node:http'
 
 const desiredPort = process.env.PORT ?? 3000
 
-const server = http.createServer((req, res) => {
-  console.log(`request received`)
-  res.end('Hello World')
-})
+const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
 
-findAvailablePort(desiredPort).then((port) => {
-  server.listen(port, () => {
-    console.log(`Server listening on port http://localhost:${port}`)
-  })
+  if (req.url === '/') {
+    res.statusCode = 200 // Ok
+    res.end('<h1>Welcome pipecoding</h1>')
+  } else if (req.url === '/contact') {
+    res.statusCode = 200 // Ok
+    res.end('<h1>luisOrtga</h1>')
+  } else {
+    res.statusCode = 404 // Not Found
+    res.setHeader('Content-Type', 'text/html; charset=utf-8') // Asegura que el navegador renderice HTML
+    res.end(`
+  <h1>404</h1>
+  <img src="https://midu.dev/images/this-is-fine-404.gif" />
+`)
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(desiredPort, () => {
+  console.log(`Server listening on port http://localhost:${desiredPort}`)
 })

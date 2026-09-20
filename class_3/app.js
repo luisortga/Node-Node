4958baad49e89b10574fe9ce8b3c28e303476a92
luisOrtga <ortegaluiskb@gmail.com>
2026-09-19 20:26:06 -0600

    Add zod and ready api

--- class_3/app.js
+++ class_3/app.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import movies from './movies/movies.json' with { type: 'json' }
+import crypto from 'node:crypto'
+import { validateMovie } from './schemas/movies.js'
 
 const app = express()
+
 app.disable('x-powered-by')
+app.use(express.json())
 
 app.get('/', (req, res) => {
   // red query params of format
@@ -10,7 +14,53 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies', (req, res) => {
+  const { genre, duration } = req.query
+
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some(
+        (g) => g.toLocaleLowerCase() === genre.toLocaleLowerCase(),
+      ),
+    )
+    return res.json(filteredMovies)
+  }
+
+  if (duration) {
+    const filteresDuration = movies.filter(
+      (movie) => movie.duration === Number(duration), // casting Number(string)
+    )
+
+    return res.json(filteresDuration)
+  }
+
   res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  // ----> path-to-regexp
+  const { id } = req.params
+  const movie = movies.find((movie) => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (!result.success) {
+    return res.status(422).json({
+      error: result.error.issues,
+    })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data,
+  }
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
 })
 
 const PORT = process.env.PORT ?? 1234
@@ -18,3 +68,5 @@ const PORT = process.env.PORT ?? 1234
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`)
 })
+
+// genre=terror : filtro or query string

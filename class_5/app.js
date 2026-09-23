e09e04fdff83c20cb9b8ddf4d33da95cb5107b71
luisOrtga <ortegaluiskb@gmail.com>
2026-09-23 11:41:32 -0600

    New class 5 with mysql

---
+++ class_5/app.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import helmet from 'helmet'
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middleware/cors.js'
+import morgan from 'morgan'
+
+const app = express()
+app.use(express.json())
+
+app.use(helmet())
+app.use(corsMiddleware())
+app.disable('x-powered-by')
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  // red query params of format
+  res.end('Movies Data')
+})
+
+app.use('/movies', moviesRouter)
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`)
+})

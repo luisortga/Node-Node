6f39609fcff0ae9889de0c30949890b6f7767624
luisOrtga <ortegaluiskb@gmail.com>
2026-09-23 17:17:38 -0600

    On ready api rest in multi database

--- class_5/app.js
+++ class_5/app.js
@@ -1,33 +1,35 @@
-import express from 'express'
+import express, { json } from 'express'
 import helmet from 'helmet'
-import { moviesRouter } from './routes/movies.js'
+import { createMovieRouter } from './routes/movies.js'
 import { corsMiddleware } from './middleware/cors.js'
 import morgan from 'morgan'
 
-const app = express()
-app.use(express.json())
+export const createApp = ({ movieModel }) => {
+  const app = express()
+  app.use(json())
 
-app.use(helmet())
-app.use(corsMiddleware())
-app.disable('x-powered-by')
-app.use(morgan('dev'))
+  app.use(helmet())
+  app.use(corsMiddleware())
+  app.disable('x-powered-by')
+  app.use(morgan('dev'))
 
-app.get('/', (req, res) => {
-  // red query params of format
-  res.end('Movies Data')
-})
+  app.get('/', (req, res) => {
+    // red query params of format
+    res.end('Movies Data')
+  })
 
-app.use('/movies', moviesRouter)
+  app.use('/movies', createMovieRouter({ movieModel }))
 
-const PORT = process.env.PORT ?? 1234
+  const PORT = process.env.PORT ?? 1234
 
-app.use((req, res) => {
-  res.status(404).send(`
+  app.use((req, res) => {
+    res.status(404).send(`
     404
     Not Found try later
   `)
-})
+  })
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`)
-})
+  app.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}`)
+  })
+}

8bdb93bc24d40eb790964a34f654942aa4b02dd7
luisOrtga <ortegaluiskb@gmail.com>
2026-09-24 20:04:18 -0600

    web socket on ready

--- class_6/app.js
+++ class_6/app.js
@@ -1,50 +1,73 @@
 import express from 'express'
 import logger from 'morgan'
-import dotenv from 'dotenv/config'
+import dotenv from 'dotenv'
 import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 
+dotenv.config()
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server, { connectionStateRecovery: {} })
+const io = new Server(server, {
+  connectionStateRecovery: {},
+})
 
 const db = createClient({
-  url: 'libsql://funny-hawkman-luisortga.aws-us-west-2.turso.io',
+  url:
+    process.env.TURSO_DATABASE_URL ??
+    'libsql://funny-hawkman-luisortga.aws-us-west-2.turso.io',
   authToken: process.env.DB_TOKEN,
 })
 
+// Crear la tabla de mensajes únicamente con id y content
 await db.execute(`
   CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     content TEXT
-  );
-  `)
+  )
+`)
 
-io.on('connection', (socket) => {
-  console.log('a user has connected')
+io.on('connection', async (socket) => {
+  console.log('a user has connected!')
 
   socket.on('disconnect', () => {
-    console.log('an user has disconnected')
+    console.log('a user has disconnected')
   })
 
   socket.on('chat message', async (msg) => {
     let result
+
     try {
       result = await db.execute({
-        sql: `INSERT INTO messages (content) VALUES (:content)`,
-        args: { content: msg },
+        sql: 'INSERT INTO messages (content) VALUES (:msg)',
+        args: { msg },
       })
-    } catch (err) {
-      console.error(err)
+    } catch (e) {
+      console.error(e)
       return
     }
 
     io.emit('chat message', msg, result.lastInsertRowid.toString())
   })
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0],
+      })
+
+      results.rows.forEach((row) => {
+        socket.emit('chat message', row.content, row.id.toString())
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 })
 
 app.use(logger('dev'))
@@ -54,5 +77,5 @@ app.get('/', (req, res) => {
 })
 
 server.listen(port, () => {
-  console.log(`Server running on port http://localhost:${port}`)
+  console.log(`Server running on port ${port}`)
 })
